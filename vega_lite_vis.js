7cfa7c19a5217f0d08b56e30ac8fbb4f5da5b0ae
Cassandra Chwee <cc.cassandrachwee@gmail.com>
2021-10-15 08:57:26 +0800

    html and js files

---
+++ vega_lite_vis.js
@@ -0,0 +1,48 @@
+var vg_1 = "teaching_scores.json";
+
+vegaEmbed("#teaching_scores_chart", vg_1).then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_2 = "research_score.json";
+
+vegaEmbed("#research_score_chart", vg_2).then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_3 = "intl_outlook.json";
+
+vegaEmbed("#intl_outlook_chart", vg_3).then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_4 = "radar.json";
+
+vegaEmbed("#radar_chart", vg_4).then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_5 = "unirank_overall.json";
+
+vegaEmbed("#unirank_overall_chart", vg_5.then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_6 = "student_num.json";
+
+vegaEmbed("#student_num_map", vg_6.then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
